c230ab7b2f966b29b12f67cdf2946961b3d476d4
shtoa <howtomatben@gmail.com>
2021-02-24 20:26:19 +0100

    first commit containing the readme.md, codebook.md, data.txt and run_analysis.R.

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Tidy_Data
+# run_analysis.R
+
+This project uses a script  called "run_analysis.R" in order to Tidy a dataset of Samsung S gyroscopic data.
+
+
+Input files (from cwd): 
+
+TEST (loc = "./UCI HAR Dataset/test"):
+
+- "./train/X_train.txt"
+- "./train/y_train.txt"
+- "./train/subject_train.txt"
+
+TRAIN (loc = "./UCI HAR Dataset/train"):
+
+- "./test/X_test.txt"
+- "./test/y_test.txt"
+- "./test/subject_test.txt"
+
+
+FEATURES (loc = "./UCI HAR Dataset"):
+
+- "features.txt": features of the dataset
+
+ACTIVITY LABELS (loc = "./UCI HAR Dataset):
+
+- "activity_labels.txt": labels for the activities (y-classifier)
+
+Output files: Generates data frame for the average of each variable for each activity and each subject
+
+Methodology: First the files are read. Then a defined function clean() is called that maps the activity label classifier to y, binds the columns of x,y and the subject as well as subseting the columns that contain mean or std measurements. The function is preformed on the testing and training datasets which are then merged to form one complete dataset. Then this dataset is grouped by activity and subject to give the final dataset for the averages for each column written to a table in the local directory under the name "data.txt".
+
+
+# Nota Bene
+
+The script requires the following packages in order to function:
+
+- tidyverse
+- data.table
+
+The data should be placed under the current workin directory in order for the script to run.
---
+++ run_analysis.R
@@ -0,0 +1,54 @@
+#Set wd to dataset
+setwd("./UCI HAR Dataset")
+
+#Reading in necessary data sets:
+
+#Features
+features <- fread(file = "features.txt")
+
+#Activity Labels
+activity_labels <- fread(file = "activity_labels.txt")
+
+#Train
+x_test <- fread(file = "./train/X_train.txt")
+y_test <- fread(file = "./train/y_train.txt")
+subject_test <- fread(file = "./train/subject_train.txt")
+
+#Test
+x_train <- fread(file = "./test/X_test.txt")
+y_train <- fread(file = "./test/y_test.txt")
+subject_train <- fread(file = "./test/subject_test.txt")
+
+
+#Create function to clean individual Train / Test sets:
+clean <- function(x,y,subject,n = features, activity = activity_labels){
+        #Change y variable to activity:
+        y <- mapvalues(y[[1]], activity[[1]] , activity[[2]])
+        
+        #Bind columns together
+        dataset <- cbind(x,y,subject)
+        
+        #Name the columns
+        names(dataset) <- c(n[[2]], "activity", "subject")
+        columns <- c(grep("mean\\(\\)|std()", names(dataset), value = TRUE),"activity", "subject")
+        
+        #Store mean and std date in subset and return
+        dataset <- subset(dataset, select = columns)
+}
+
+#Creates Clean Data
+
+clean_train <- clean(x_test, y_test, subject_test)
+clean_test <- clean(x_train, y_train, subject_train)
+
+#Generates the final tidy dataframe
+data <- rbind(clean_train, clean_test)
+
+#Generates data frame for the average of each variable for each activity and each subject
+activity_subject_data <- summarise_all(group_by(data, activity, subject), mean)
+
+#Set wd to previous directory
+setwd("../")
+
+#Writes Table for the average of each variable for each activity and each subject
+write.table(activity_subject_data,"data.txt",col.names = T, row.name = F)
